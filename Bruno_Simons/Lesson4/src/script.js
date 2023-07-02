310fdde36e10d66d55ccef0e5ee20277f8e9abab
Terra-01 <thisisshivam@icloud.com>
2023-07-02 13:28:29 +0530

    lesson 4 in Vite

--- Bruno_Simons/Lesson4/src/script.js
+++ Bruno_Simons/Lesson4/src/script.js
@@ -1,38 +1,28 @@
-import './style.css'
 import * as THREE from 'three'
 
-console.log(THREE)
+//Sizes
+const sizes = {
+    width:800,
+    height:600
+}
 
-// We need to create a scene :
-// For which we need object, camera and a renderer.
+//Scene
 const scene = new THREE.Scene()
 
-// Object Red Cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xffdfdf })
+//Camera
+const camera = new THREE.PerspectiveCamera(55, sizes.width/sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+//Cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-// camera.position.x = 1
-// camera.position.y = 1
-scene.add(camera)
-
-
-// Renderer
-// Getting the canvas element of class webgl from the HTML file
-const canvas = document.querySelector('.webgl')
-// console.log(canvas)
+//Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: document.querySelector('canvas.webgl')
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
