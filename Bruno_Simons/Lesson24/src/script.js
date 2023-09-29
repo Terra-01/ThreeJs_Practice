903facf133ab34c539979cd69dceae5e4372edda
Terra-01 <thisisshivam@icloud.com>
2023-09-29 16:32:32 +0530

    Lesson24

--- Bruno_Simons/Lesson24/src/script.js
+++ Bruno_Simons/Lesson24/src/script.js
@@ -1,20 +1,16 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import * as dat from 'lil-gui'
 
-/**
- * Loader
- */
-const gltfLoader = new GLTFLoader()
-const cubeTextureloader = new THREE.CubeTextureLoader()
+THREE.ColorManagement.enabled = false
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
-const global = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -23,71 +19,55 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Update all materials
- */
-const updateAllmaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material.isMeshStandardMaterial)
-        {
-            // console.log(child)
-            child.material.envMapIntensity = global.envMapIntensity
-        }
-    })
-}
-
-/**
- * Environment Map
- */
-// Global intensity
-global.envMapIntensity = 1
-gui
-    .add(global, 'envMapIntensity')
-    .min(0) 
-    .max(10) 
-    .step(0.001)
-    .onChange(updateAllmaterials)
-
-//LDR cube Texture
-const environmentMap = cubeTextureloader.load([
-    '/environmentMaps/0/px.png',
-    '/environmentMaps/0/nx.png',
-    '/environmentMaps/0/py.png',
-    '/environmentMaps/0/ny.png',
-    '/environmentMaps/0/pz.png',
-    '/environmentMaps/0/nz.png'
-])
-
-scene.environment = environmentMap
-scene.background = environmentMap
-
-scene.background = environmentMap
-/**
- * Torus Knot
- */
-const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-    new THREE.MeshStandardMaterial({ roughness: 0.3, metalness: 1, color: 0xaaaaaa })
-)
-torusKnot.position.x = - 4
-torusKnot.position.y = 4
-scene.add(torusKnot)
-
-/**
  * Models
  */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
 gltfLoader.load(
-    'models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/hamburger.glb',
     (gltf) =>
     {
-        gltf.scene.scale.set(10, 10, 10)
         scene.add(gltf.scene)
-
-        updateAllmaterials()
     }
-
 )
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(50, 50),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -117,12 +97,12 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 5, 4)
+camera.position.set(- 8, 4, 8)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3.5
+controls.target.set(0, 1, 0)
 controls.enableDamping = true
 
 /**
@@ -131,6 +111,9 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -138,10 +121,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
+
 const tick = () =>
 {
-    // Time
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    if(mixer)
+    {
+        mixer.update(deltaTime)
+    }
 
     // Update controls
     controls.update()
