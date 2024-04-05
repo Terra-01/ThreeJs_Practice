06b1fe3c6915404f84d635ddac19f67a47ea17e4
Terra-01 <thisisshivam@icloud.com>
2024-04-05 13:07:57 +0530

    commit

--- Bruno_Simons/Lesson29/src/shaders/test/fragment.glsl
+++ Bruno_Simons/Lesson29/src/shaders/test/fragment.glsl
@@ -5,6 +5,14 @@ float random(vec2 st)
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main()
 {
 
@@ -186,9 +194,29 @@ void main()
     // float strength = 0.015 / distance(lightvUv, vec2(0.5));
     // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
+    // Both Same but different but still the same
+    // vec2 lightvUv = vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5);
+    // float strength = 0.15 / distance(lightvUv, vec2(0.5));
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
     // Pattern 31
-    vec2 lightvUv = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
-    float strength = 0.015 / distance(lightvUv, vec2(0.5));
+    // vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    // float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+    
+    // vec2 lightUvY = vec2(vUv.x * 0.5 + 0.25, vUv.y * 0.1 + 0.45);
+    // float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    
+    // float strength = lightX * lightY;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+    
+    // Pattern 32
+    vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+    
+    vec2 lightUvY = vec2(vUv.x * 0.5 + 0.25, vUv.y * 0.1 + 0.45);
+    float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+    
+    float strength = lightX * lightY;
     gl_FragColor = vec4(strength, strength, strength, 1.0);
     
 }
